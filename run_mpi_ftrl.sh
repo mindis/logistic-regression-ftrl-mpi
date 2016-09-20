c51a4d8b4af8c22ddee08ca227de082e5cfa7619
wangxiaoshu <wangxiaoshu@yidian-inc.com>
2016-09-20 13:33:13 +0800

    add data

--- run_mpi_ftrl.sh
+++ run_mpi_ftrl.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 process_number=3
-Ip=("10.101.2.89" "10.101.2.90")
+Ip=("0.0.0.0" "0.0.0.0")
 for ip in ${Ip[@]}
 do
     ssh worker@$ip rm /home/worker/xiaoshu/logistic-regression-ftrl-mpi/train
+    scp train worker@$ip:/home/worker/xiaoshu/logistic-regression-ftrl-mpi/.
 done
-scp train worker@10.101.2.89:/home/worker/xiaoshu/logistic-regression-ftrl-mpi/.
-scp train worker@10.101.2.90:/home/worker/xiaoshu/logistic-regression-ftrl-mpi/.
-mpirun -f ./hosts -np $process_number ./train ftrl 1000 500 0.0 0.1 1.0 0.001 0.0 ./data/v2v_train ./data/v2v_test
-#mpirun -f ./hosts -np $process_number ./train ftrl 1 500 1.0 1.0 1.0 0.1 1.0 ./data/traindataold ./data/testdataold
-#mpirun -f ../hosts -np $process_number ./train ftrl ./data/agaricus.txt.train ./data/agaricus.txt.test
+mpirun -f ../hosts -np $process_number ./train ftrl 10 10 0.0 0.1 1.0 0.001 0.0 ./data/agaricus.txt.train ./data/agaricus.txt.test
